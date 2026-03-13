0a205055720d5a996cebb859a57879bb98610cf5
Iain Robertson <iain_robertson@hotmail.com>
2026-03-14 09:09:30 +1000

    New wristbands.py commit

---
+++ wristbands.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# 
+# $Id$
+#
+# Takes a CSV file with the following values and converts it to wristbands.  Wrist band size is 297mm by 21mm - 10 to an A4 sheet (landscape).
+# car_number, surname, firstname
+# These are then saved to a PDF, one to a band, with car number -> firstname lastname 
+# Typeface is Helvetica, font size is 28 points.
+# uses https://py-pdf.github.io/fpdf2/ for generation.
+# 
+# to run:
+# env python3 wristbands.py input.csv output.pdf type
+from fpdf import FPDF
+import sys
+import csv 
+
+if len(sys.argv) != 4:
+    sys.stderr.write("Error: incorrect usage\n")
+    sys.stderr.write("Usage: " + sys.argv[0] + " input.csv output.pdf type")
+    exit(1)
+
+pdf = FPDF(orientation="landscape", format="A4")
+pdf.set_margin(0)
+pdf.add_page()
+
+with open(sys.argv[1], newline='') as csvfile:
+    field_names = ["carnumber", "surname", "firstname"]
+    entries = csv.DictReader(csvfile, fieldnames=field_names)
+    for row in entries:
+        pdf.set_font("helvetica", size=26, style="B")
+        line = row["carnumber"] + "\t" + row["firstname"] + " " + row["surname"]
+        pdf.cell(w=297, h=15, text=line.upper(), align="C", new_x="LEFT", new_y="NEXT")
+        pdf.set_font("helvetica", size=12, style="B")
+        pdf.cell(w=297, h=6, text=sys.argv[3].upper(), align="C", new_x="LEFT", new_y="NEXT")
+
+pdf.output(sys.argv[2])
+
+
+exit(0)
